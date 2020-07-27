de1ae505cbbdb8707d5dfe1883ed209fa1ab535b
Luke <lmschless@gmail.com>
2020-07-27 11:56:20 -0700

    updated readme and deployed to gh-pages

---
+++ build/precache-manifest.0238e279c14fa9a0838e9a0a4324d992.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57731592b49e5ac6043323408212e288",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "e867d690e4d0e7557384",
+    "url": "/react-todo-list/static/css/main.1cf6598a.chunk.css"
+  },
+  {
+    "revision": "8dba8b139f1fb98cfb12",
+    "url": "/react-todo-list/static/js/2.0099febd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.0099febd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e867d690e4d0e7557384",
+    "url": "/react-todo-list/static/js/main.af61c7d5.chunk.js"
+  },
+  {
+    "revision": "2c6e5d8f9eaaedf40455",
+    "url": "/react-todo-list/static/js/runtime-main.f23babde.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
