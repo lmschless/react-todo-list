ceee9f06c315ff267f3462bdfb99958e85e25b74
Luke <lmschless@gmail.com>
2020-07-27 11:51:28 -0700

    Updates

---
+++ precache-manifest.9ebae2d8d69a01e05f227be3e92ed8f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b73c512bb87aa68d4d3943e8f80ea2d",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "2f8be444d5287ef9be8b",
+    "url": "/react-todo-list/static/css/main.1cf6598a.chunk.css"
+  },
+  {
+    "revision": "c5c86ff922da622c3630",
+    "url": "/react-todo-list/static/js/2.09079300.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.09079300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f8be444d5287ef9be8b",
+    "url": "/react-todo-list/static/js/main.1b2369f2.chunk.js"
+  },
+  {
+    "revision": "c20723967e7f6bc2ae1a",
+    "url": "/react-todo-list/static/js/runtime-main.f468bcb5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
